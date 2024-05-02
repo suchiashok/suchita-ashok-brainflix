f428c162aa8972a68f0638d3254979e7294939b2
suchi ashok <suchiashok.97@gmail.com>
2024-05-02 10:54:56 -0400

    Updating the partials and header for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss'
+import Header from './components/Component/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
---
+++ src/components/Component/Header.jsx
@@ -0,0 +1,48 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img
+          className="header__logoImage"
+          src="/src/assets/Logo/BrainFlix-logo.svg"
+          alt="brainflix-logo"
+        />
+      </div>
+      <div className="header__sideElements">
+        <form className="header__searchInput">
+          <input
+            className="header__searchField"
+            type="search"
+            id="searchInput"
+            name="SearchInput"
+            placeholder="Search"
+          />
+          <img
+            className="header__searchIcon"
+            src="/src/assets/Icons/search.svg"
+            alt="search-icon"
+          ></img>
+        </form>
+        <img
+          className="header__avatar"
+          src="/src/assets/Images/Mohan-muruge.jpg"
+          alt="avatar-image"
+        ></img>
+      </div>
+      <div className="header__button">
+        <button className="header__buttonEl">
+          <img
+            className="header__buttonIcon"
+            src="/src/assets/Icons/upload.svg"
+            alt="upload-icon"
+          ></img>
+          UPLOAD
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;

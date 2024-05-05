be8c2fa6187ffe8d23183358061d8ccfcaa4f2c3
suchi ashok <suchiashok.97@gmail.com>
2024-05-05 18:11:09 -0400

    Updatingtab styles for comments and nextVideos

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -10,20 +10,25 @@ const CommentsForm = () => {
         ></img>
       </div>
       <div className="form__formButton">
-        <form className="form__formEl">
-          <label className="form__label" htmlFor="comments">
-            JOIN THE CONVERSATION
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="comments"
-            name="comments"
-            placeholder="Add a new comment"
-          ></input>
-        </form>
-        <button className="form__button">
-            <img src="/src/assets/Icons/add_comment.svg"></img>COMMENT</button>
+        <div className="form__mainForm">
+          <form className="form__formEl">
+            <label className="form__label" htmlFor="comments">
+              JOIN THE CONVERSATION
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              id="comments"
+              name="comments"
+              placeholder="Add a new comment"
+            ></input>
+          </form>
+        </div>
+        <div className="form__mainButton">
+          <button className="form__button">
+            <img src="/src/assets/Icons/add_comment.svg"></img>COMMENT
+          </button>
+        </div>
       </div>
     </div>
   );

cc763417ffb74ae02397921c6a31974ab4f91142
suchi ashok <suchiashok.97@gmail.com>
2024-05-09 00:08:53 -0400

    Adding all the elements for Upload Video page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 import Home from "./pages/Home/Home";
 import UploadVideo from "./components/UploadVideo/UploadVideo";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const BASE_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
+const API_KEY = "98457a44-b052-41f0-a7e8-25093f568c3f";
 
 function App() {
+  const [ids, setIds] = useState(null);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  if (!ids) {
+    return <div className="loader">loading...</div>;
+  }
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='upload' element={<UploadVideo />} />
+          <Route path="/" element={<Home ids={ids} />} />
+          <Route path="/videos/:id" element={<Home id={ids} />} />
+          <Route path="upload" element={<UploadVideo />} />
         </Routes>
       </BrowserRouter>
     </>
   );
+
+  async function getVideos() {
+    try {
+      const response = await axios.get(`${BASE_URL}videos?api_key=${API_KEY}`);
+      setIds(response.data.id);
+    } catch {
+      console.log("Error fetching the videos");
+    }
+  }
 }
 
 export default App;
--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -5,6 +5,43 @@ function UploadVideo() {
   return (
     <>
       <Header />
+      <div className="upload">
+        <h3 className="upload__title">Upload Video</h3>
+        <form className="upload__form">
+          <label className="upload__label">VIDEO THUMBNAIL</label>
+          <img
+            className="upload__image"
+            src="/src/assets/Images/Upload-video-preview.jpg"
+          ></img>
+          <label className="upload__label">TITLE YOUR VIDEO</label>
+          <input
+            className="upload__input"
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Add a title to your video"
+          ></input>
+          <label className="upload__label">
+            ADD A VIDEO DESCRIPTION
+            <input
+              className="upload__input"
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Add a description to your video"
+            ></input>
+          </label>
+          <button className="header__buttonEl">
+            <img
+              className="upload__publishButton"
+              src="/src/assets/Icons/publish.svg"
+              alt="upload-icon"
+            ></img>
+            PUBLISH
+          </button>
+          <button className="upload__cancelButton">CANCEL</button>
+        </form>
+      </div>
     </>
   );
 }

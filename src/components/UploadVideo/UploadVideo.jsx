ccb843fb284c652aee2b03edced5542f87c948de
suchi ashok <suchiashok.97@gmail.com>
2024-05-12 21:32:04 -0400

    Updating styles - Desktop

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -13,39 +13,45 @@ function UploadVideo() {
       <div className="upload">
         <h3 className="upload__title">Upload Video</h3>
         <form className="upload__form" onSubmit={handleSubmit}>
-          <label className="upload__label">VIDEO THUMBNAIL</label>
-          <img
-            className="upload__image"
-            src="/src/assets/Images/Upload-video-preview.jpg"
-          ></img>
-          <label className="upload__label">TITLE YOUR VIDEO</label>
-          <input
-            className="upload__input1"
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Add a title to your video"
-          ></input>
-          <label className="upload__label">
-            ADD A VIDEO DESCRIPTION
-            <input
-              className="upload__input2"
-              type="text"
-              id="description"
-              name="description"
-              placeholder="Add a description to your video"
-            ></input>
-          </label>
-          <div className="upload__buttons">
-          <button className="upload__publishButton" type="submit">
-            <img
-              className="upload__publishIcon"
-              src="/src/assets/Icons/publish.svg"
-              alt="upload-icon"
-            ></img>
-            PUBLISH
-          </button>
-          <button className="upload__cancelButton">CANCEL</button>
+          <div className="upload__imageInputs">
+            <div className="upload__imageDiv">
+              <label className="upload__label">VIDEO THUMBNAIL</label>
+              <img
+                className="upload__image"
+                src="/src/assets/Images/Upload-video-preview.jpg"
+              ></img>
+            </div>
+            <div className="upload__inputsDiv">
+              <label className="upload__label">TITLE YOUR VIDEO</label>
+              <input
+                className="upload__input1"
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Add a title to your video"
+              ></input>
+              <label className="upload__label">
+                ADD A VIDEO DESCRIPTION
+                <input
+                  className="upload__input2"
+                  type="text"
+                  id="description"
+                  name="description"
+                  placeholder="Add a description to your video"
+                ></input>
+              </label>
+            </div>
+          </div>
+          <div className="upload__buttonsDiv">
+            <button className="upload__publishButton" type="submit">
+              <img
+                className="upload__publishIcon"
+                src="/src/assets/Icons/publish.svg"
+                alt="upload-icon"
+              ></img>
+              PUBLISH
+            </button>
+            <button className="upload__cancelButton">CANCEL</button>
           </div>
         </form>
       </div>
